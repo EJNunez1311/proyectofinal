e59a22693631caa048e55ea0a87d669332b950cd
Edgar Núñez <39830766+EJNunez1311@users.noreply.github.com>
2020-11-12 08:29:09 -0400

    Class App Name y DBInfo

--- src/main/java/Entities/AppName.java
+++ src/main/java/Entities/AppName.java
@@ -1,11 +1,17 @@
 package Entities;
 
-import javax.ws.rs.FormParam;
 
 public class AppName {
 
-    @FormParam("name")
-    private String name;
+    public String name;
+    public boolean securityCheckbox;
+    public boolean microserviceCheckbox;
+
+    public AppName(String name, boolean securityCheckbox, boolean microserviceCheckbox) {
+        this.name = name;
+        this.securityCheckbox = securityCheckbox;
+        this.microserviceCheckbox = microserviceCheckbox;
+    }
 
     public String getName() {
         return name;
@@ -13,5 +19,21 @@ public class AppName {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isSecurityCheckbox() {
+        return securityCheckbox;
+    }
+
+    public void setSecurityCheckbox(boolean securityCheckbox) {
+        this.securityCheckbox = securityCheckbox;
+    }
+
+    public boolean isMicroserviceCheckbox() {
+        return microserviceCheckbox;
+    }
+
+    public void setMicroserviceCheckbox(boolean microserviceCheckbox) {
+        this.microserviceCheckbox = microserviceCheckbox;
     }
 }
